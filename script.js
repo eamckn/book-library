d1e76ac66945ed6a3e6540d348cc34e78bb370dd
Eamon McKeon <154269556+eamckn@users.noreply.github.com>
2024-07-17 10:57:30 -0400

    Add function to add book to library w user input

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 // JS for book library project
 
-// Object constructors
+// Initialize library, i.e an array of books
+const myLibrary = [];
+
+// Object constructor
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
+}
+
+// Function definitions
+function addBookToLibrary() {
+    let title = prompt("What is the title of the book you'd like to add to your library?");
+    let author = prompt("What's the name of the author?");
+    let pages = prompt("How many pages is the book? (please enter a number)");
+    let read = prompt("Have you had this book already? Please answer 'yes' or 'no.'");
+    let newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook)
 }
