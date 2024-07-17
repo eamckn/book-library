cc931c132167275857669eac61cc240d4ad39a3c
Eamon McKeon <154269556+eamckn@users.noreply.github.com>
2024-07-17 10:41:53 -0400

    Add Book constructor

---
+++ script.js
@@ -0,0 +1,9 @@
+// JS for book library project
+
+// Object constructors
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
